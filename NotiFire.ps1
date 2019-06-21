ff3c1781ff136aaeb112597c0a8f13b4b50a03e7
Noran Diaconu <diaconuna@appstate.edu>
2019-06-21 16:34:16 -0400

    Display notification when text within folder has changed

--- NotiFire.ps1
+++ NotiFire.ps1
@@ -5,12 +5,40 @@ $Main_Tool_Icon.Text = "NotiFire";
 $Main_Tool_Icon.Icon = "ico.ico";
 $Main_Tool_Icon.BalloonTipIcon = "None";
 $Main_Tool_Icon.BalloonTipTitle = "Break Time";
-$Main_Tool_Icon.Visible = $True;
+$Main_Tool_Icon.Visible = $true;
 
 $Main_Tool_Icon.Add_Click({
+    Unregister-Event -SourceIdentifier Change;
+    $watcher.EnableRaisingEvents = $false;
+    $watcher.Dispose();
 	$Main_Tool_Icon.Visible = $false;
     Stop-Process -Id $pid;
 })
+
+$path = "c:\folderPath"
+explorer $path
+
+$watcher = New-Object System.IO.FileSystemWatcher;
+$watcher.Path  = $path;
+$watcher.Filter = "*.txt";
+$watcher.EnableRaisingEvents = $true;
+
+$action = {
+    $Main_Tool_Icon2 = New-Object System.Windows.Forms.NotifyIcon;
+    $Main_Tool_Icon2.Text = "NotiFire";
+    $Main_Tool_Icon2.Icon = "ico.ico";
+    $Main_Tool_Icon2.BalloonTipIcon = "None";
+    $Main_Tool_Icon2.BalloonTipTitle = "New Message";
+    $Main_Tool_Icon2.Visible = $true;
+
+    $Main_Tool_Icon2.Add_Click({
+	    $Main_Tool_Icon2.Visible = $false;
+    })
+    $Main_Tool_Icon2.BalloonTipText = "New Message";
+    $Main_Tool_Icon2.ShowBalloonTip(20000);
+}
+
+Register-ObjectEvent -InputObject $watcher -EventName Changed -Action $action -SourceIdentifier Change;
 
 while ($true) {
     $theTime = Get-Date -format HH:mm;
@@ -18,5 +46,5 @@ while ($true) {
         $Main_Tool_Icon.BalloonTipText = $theTime;
         $Main_Tool_Icon.ShowBalloonTip(20000);
     }
-    Start-Sleep 5;
+    Wait-Event -Timeout 5;
 }
--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# NotiFire - 1.1
+# NotiFire - 1.2
 Run the vbs and notifications will be created for Break Time&trade;.
+
+Display notifications when the text within a folder has changed.
 
 Close the program by clicking on the icon in the system tray.
 
-## New in 1.1
+## New in 1.2
+Display notifications when the text within a folder has changed.
+
+### NotiFire - 1.1
 The command prompt is no longer open while running.
 
 The notification icon is persistent while running and is no longer duplicated for each notification.
+
+### NotiFire - 1.0
+Initial release
